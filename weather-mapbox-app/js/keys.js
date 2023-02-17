64e0de96ee6a548ba3e9a5a8320f5e9201c34602
Johnny Wells <120209205+johnnyw001@users.noreply.github.com>
2023-02-17 13:40:30 -0600

    Delete keys.js

--- weather-mapbox-app/js/keys.js
+++
@@ -1,5 +0,0 @@
-const keys = {
-  mapbox: 'pk.eyJ1IjoiandlbGxzMDEiLCJhIjoiY2xkM242aTJkMGszaDNwcWwxdGV4NDdibSJ9.wuVFoWvnOHMFAfbWt-u66A',
-  openWeather: '17f468b4729608724d38a55da4c16973'
-}
-console.log("success");
